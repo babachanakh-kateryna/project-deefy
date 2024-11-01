c56fdbc60ea629c5431e2213e68659050550c7eb
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-11-01 19:56:29 +0100

    modification de la classe AddTrackAction

--- src/classes/action/AddTrackAction.php
+++ src/classes/action/AddTrackAction.php
@@ -2,6 +2,7 @@
 
 namespace iutnc\deefy\action;
 
+use getID3;
 use iutnc\deefy\audio\tracks as tracks;
 use iutnc\deefy\render as render;
 use iutnc\deefy\repository\DeefyRepository;
@@ -25,16 +26,39 @@ class AddTrackAction extends Action
     {
         return <<<HTML
 <form method="post" action="?action=add-track" enctype="multipart/form-data">
+    <label for="type">Track Type:</label>
+    <select id="type" name="type">
+        <option value="P">Podcast</option>
+        <option value="A">Album Track</option>
+    </select>
+    
     <label for="title">Title:</label>
     <input type="text" id="title" name="title" required>
-    <label for="author">Author:</label>
+
+    <label for="author">Author (for Podcasts):</label>
     <input type="text" id="author" name="author">
-    <label for="date">Date:</label>
+
+    <label for="date">Date (for Podcasts):</label>
     <input type="text" id="date" name="date">
+
     <label for="genre">Genre:</label>
     <input type="text" id="genre" name="genre">
+
+    <label for="artist">Artist (for Album Tracks):</label>
+    <input type="text" id="artist" name="artist">
+
+    <label for="albumTitle">Album Title (for Album Tracks):</label>
+    <input type="text" id="albumTitle" name="albumTitle">
+
+    <label for="year">Year (for Album Tracks):</label>
+    <input type="number" id="year" name="year">
+
+    <label for="trackNumber">Track Number (for Album Tracks):</label>
+    <input type="number" id="trackNumber" name="trackNumber">
+
     <label for="file">Audio File (.mp3):</label>
     <input type="file" id="file" name="userfile" accept=".mp3,audio/mpeg">
+    
     <button type="submit">Add Track</button>
 </form>
 HTML;
@@ -66,26 +90,56 @@ HTML;
 
         // generer un nouveau nom de fichier et enregistrer le fichier
         $newFilename = uniqid() . '.mp3';
-        $destination = __DIR__ . '/audio/music/' . $newFilename;
+        $destination = __DIR__ . '/../../../music/' . $newFilename;
 
         if (!move_uploaded_file($file['tmp_name'], $destination)) {
             return "<div>Error: Failed to save file.</div>";
         }
 
+        // utiliser getID3 pour obtenir la duree du fichier
+        $getID3 = new getID3();
+        $fileInfo = $getID3->analyze($destination);
+        $duration = isset($fileInfo['playtime_seconds']) ? (int)$fileInfo['playtime_seconds'] : 0;
+
+        // verifier la duree du fichier
+        if ($duration <= 0) {
+            return "<div>Error: The uploaded audio file has an invalid duration.</div>";
+        }
+
         // file path PodcastTrack
         $repo = DeefyRepository::getInstance();
-        $file_path = "audio/music/" . $newFilename;
+        $file_path = "music/" . $newFilename;
 
+        // recuperer les donnees du formulaire
+        $type = filter_input(INPUT_POST, 'type', FILTER_SANITIZE_SPECIAL_CHARS);
         $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
-        $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_SPECIAL_CHARS);
-        $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
         $genre = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_SPECIAL_CHARS);
 
-        $track = new tracks\PodcastTrack($title, $file_path, 0);
-        $track->setAuteur($author);
-        $track->setDate($date);
-        $track->setGenre($genre);
-        $track = $repo->savePodcastTrack($track);
+        // creation d'un objet piste en fonction du type
+        if ($type === 'P') {
+            $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_SPECIAL_CHARS);
+            $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+
+            // PodcastTrack
+            $track = new tracks\PodcastTrack($title, $file_path, $duration);
+            $track->setAuteur($author);
+            $track->setDate($date);
+            $track->setGenre($genre);
+
+            $savedTrack = $repo->saveTrack($track, 'P');
+        } elseif ($type === 'A') {
+            $artist = filter_input(INPUT_POST, 'artist', FILTER_SANITIZE_SPECIAL_CHARS);
+            $albumTitle = filter_input(INPUT_POST, 'albumTitle', FILTER_SANITIZE_SPECIAL_CHARS);
+            $year = filter_input(INPUT_POST, 'year', FILTER_VALIDATE_INT);
+            $trackNumber = filter_input(INPUT_POST, 'trackNumber', FILTER_VALIDATE_INT);
+
+            // AlbumTrack
+            $track = new tracks\AlbumTrack($title, $file_path, $albumTitle, $trackNumber, $duration, $artist, $year, $genre);
+
+            $savedTrack = $repo->saveTrack($track, 'A');
+        } else {
+            return "<div>Error: Invalid track type selected.</div>";
+        }
 
         $playlist = $_SESSION['current_playlist'];
         $repo->addTrackToPlaylist($playlist->id, $track->id, $playlist->nombrePistes + 1);
