1358c7f9c2c55ec65dd1773fec2d72b5ab7d9dd4
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-11-02 01:21:45 +0100

    Ajout bootstrap

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a1046c938c1e1cecbd194d9de7d1ba4d1714dea5',
+        'reference' => 'c9b3c337f76ae246cacf509ff1307bcf46088b14',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a1046c938c1e1cecbd194d9de7d1ba4d1714dea5',
+            'reference' => 'c9b3c337f76ae246cacf509ff1307bcf46088b14',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -27,6 +27,21 @@
             'install_path' => __DIR__ . '/../james-heinrich/getid3',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.3',
+            'version' => '5.3.3.0',
+            'reference' => '6e1f75f420f68e1d52733b8e407fc7c3766c9dba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.3',
+            ),
         ),
     ),
 );
